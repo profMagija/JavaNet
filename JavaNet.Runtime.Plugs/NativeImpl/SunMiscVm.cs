184afb95444389bdd5d7d7e3c408ded20eb7d364
Nikola Bebic <nikola.bebic99@gmail.com>
2019-04-02 12:28:50 +0200

    add sun.misc.VM native implementations

---
+++ JavaNet.Runtime.Plugs/NativeImpl/SunMiscVm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JavaNet.Runtime.Plugs.NativeImpl
+{
+    public static class SunMiscVm
+    {
+        public const string TypeName = "sun.misc.VM";
+
+        [NativeImpl(typeof(void), TypeName, "initialize", IsStatic = true)]
+        public static void Initialize()
+        {
+
+        }
+
+        [NativeImpl("java.lang.ClassLoader", TypeName, "latestUserDefinedLoader", IsStatic = true)]
+        [return: ActualType("java.lang.ClassLoader")]
+        public static object LatestUserDefinedLoader()
+        {
+            return null;
+        }
+
+
+    }
+}
