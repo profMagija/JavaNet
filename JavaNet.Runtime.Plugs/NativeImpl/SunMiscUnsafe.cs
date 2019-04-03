28c3d93605f2ed8e0a27191b4759574964581833
Nikola Bebic <nikola.bebic99@gmail.com>
2019-04-03 18:59:49 +0200

    add sun.misc.Unsafe native methods

---
+++ JavaNet.Runtime.Plugs/NativeImpl/SunMiscUnsafe.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JavaNet.Runtime.Plugs.NativeImpl
+{
+    public static class SunMiscUnsafe
+    {
+        public const string TypeName = "sun.misc.Unsafe";
+
+        [NativeImpl(typeof(void), TypeName, "registerNatives", IsStatic = true)]
+        public static void RegisterNatives()
+        {
+        }
+    }
+}
