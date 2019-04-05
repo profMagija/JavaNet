03c7967cb70ba67f9260a6fa78b07304d0ea5dcd
Nikola Bebic <nikola.bebic99@gmail.com>
2019-04-05 12:05:20 +0200

    more sun.misc.Unsafe stuff

--- JavaNet.Runtime.Plugs/NativeImpl/SunMiscUnsafe.cs
+++ JavaNet.Runtime.Plugs/NativeImpl/SunMiscUnsafe.cs
@@ -59,6 +59,70 @@ namespace JavaNet.Runtime.Plugs.NativeImpl
             Marshal.WriteInt32(ptr, (int) offset, value);
         }
 
+        [NativeImpl(typeof(int), TypeName, "getInt", typeof(long))]
+        public static int getInt(object @this, long ptr)
+        {
+            return Marshal.ReadInt32(new IntPtr(ptr));
+        }
+
+        [NativeImpl(typeof(void), TypeName, "putInt", typeof(long), typeof(int))]
+        public static void putInt(object @this, long ptr, int value)
+        {
+            Marshal.WriteInt32(new IntPtr(ptr), value);
+        }
+
+        [NativeImpl(typeof(long), TypeName, "getLongVolatile", typeof(object), typeof(long))]
+        [NativeImpl(typeof(long), TypeName, "getLong", typeof(object), typeof(long))]
+        public static long getLong(object @this, object ptr, long offset)
+        {
+            return Marshal.ReadInt64(ptr, (int)offset);
+        }
+
+        [NativeImpl(typeof(void), TypeName, "putLongVolatile", typeof(object), typeof(long), typeof(long))]
+        [NativeImpl(typeof(void), TypeName, "putLong", typeof(object), typeof(long), typeof(long))]
+        public static void putLong(object @this, object ptr, long offset, long value)
+        {
+            Marshal.WriteInt64(ptr, (int)offset, value);
+        }
+
+        [NativeImpl(typeof(long), TypeName, "getLong", typeof(long))]
+        public static long getLong(object @this, long ptr)
+        {
+            return Marshal.ReadInt64(new IntPtr(ptr));
+        }
+
+        [NativeImpl(typeof(void), TypeName, "putLong", typeof(long), typeof(long))]
+        public static void putLong(object @this, long ptr, long value)
+        {
+            Marshal.WriteInt64(new IntPtr(ptr), value);
+        }
+
+        [NativeImpl(typeof(sbyte), TypeName, "getByteVolatile", typeof(object), typeof(long))]
+        [NativeImpl(typeof(sbyte), TypeName, "getByte", typeof(object), typeof(long))]
+        public static long getByte(object @this, object ptr, long offset)
+        {
+            return (sbyte)Marshal.ReadByte(ptr, (int)offset);
+        }
+
+        [NativeImpl(typeof(void), TypeName, "putByteVolatile", typeof(object), typeof(long), typeof(sbyte))]
+        [NativeImpl(typeof(void), TypeName, "putByte", typeof(object), typeof(long), typeof(sbyte))]
+        public static void putByte(object @this, object ptr, long offset, sbyte value)
+        {
+            Marshal.WriteByte(ptr, (int) offset, (byte) value);
+        }
+
+        [NativeImpl(typeof(sbyte), TypeName, "getByte", typeof(long))]
+        public static sbyte getByte(object @this, long ptr)
+        {
+            return (sbyte)Marshal.ReadByte(new IntPtr(ptr));
+        }
+
+        [NativeImpl(typeof(void), TypeName, "putByte", typeof(long), typeof(sbyte))]
+        public static void putByte(object @this, long ptr, sbyte value)
+        {
+            Marshal.WriteByte(new IntPtr(ptr), (byte) value);
+        }
+
         [NativeImpl(typeof(bool), TypeName, "compareAndSwapInt", typeof(object), typeof(long), typeof(int), typeof(int))]
         public static bool compareAndSwapInt(object @this, object ptr, long offset, int value, int original)
         {
@@ -69,6 +133,26 @@ namespace JavaNet.Runtime.Plugs.NativeImpl
                 return read != original;
             }
         }
+
+        [NativeImpl(typeof(long), TypeName, "allocateMemory", typeof(long))]
+        public static long allocateMemory(object @this, long size)
+        {
+            return Marshal.AllocHGlobal(new IntPtr(size)).ToInt64();
+        }
+
+        [NativeImpl(typeof(void), TypeName, "freeMemory", typeof(long))]
+        public static void freeMemory(object @this, long ptr)
+        {
+            Marshal.FreeHGlobal(new IntPtr(ptr));
+        }
+
+        [NativeImpl(typeof(long), TypeName, "reallocateMemory", typeof(long), typeof(long))]
+        public static long reallocateMemory(object @this, long ptr, long newSize)
+        {
+            return Marshal.ReAllocHGlobal(new IntPtr(ptr), new IntPtr(newSize)).ToInt64();
+        }
+
+
 
     }
 }
