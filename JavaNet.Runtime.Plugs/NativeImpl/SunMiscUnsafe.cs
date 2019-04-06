91b6eb7dc3b27b0b738627dc7145c127d2c03840
Nikola Bebic <nikola.bebic99@gmail.com>
2019-04-06 09:30:11 +0200

    more unsafe natives

--- JavaNet.Runtime.Plugs/NativeImpl/SunMiscUnsafe.cs
+++ JavaNet.Runtime.Plugs/NativeImpl/SunMiscUnsafe.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
@@ -12,19 +13,19 @@ namespace JavaNet.Runtime.Plugs.NativeImpl
     {
         public const string TypeName = "sun.misc.Unsafe";
 
-        [NativeImpl(typeof(void), TypeName, "registerNatives", IsStatic = true)]
-        public static void RegisterNatives()
+        [NativeImpl(IsStatic = true)]
+        public static void registerNatives()
         {
         }
 
-        [NativeImpl(typeof(int), TypeName, "arrayBaseOffset", typeof(Type))]
-        public static int ArrayBaseOffset(object @this, Type t)
+        [NativeImpl]
+        public static int arrayBaseOffset(object @this, Type t)
         {
             return 0;
         }
 
-        [NativeImpl(typeof(int), TypeName, "arrayIndexScale", typeof(Type))]
-        public static int ArrayIndexScale(object @this, Type t)
+        [NativeImpl]
+        public static int arrayIndexScale(object @this, Type t)
         {
             var elementType = t.GetElementType();
             if (elementType == null)
@@ -32,98 +33,131 @@ namespace JavaNet.Runtime.Plugs.NativeImpl
             return elementType.IsValueType ? Marshal.SizeOf(elementType) : Marshal.SizeOf<IntPtr>();
         }
 
-        [NativeImpl(typeof(int), TypeName, "addressSize")]
-        public static int AddressSize(object @this)
+        [NativeImpl]
+        public static int addressSize(object @this)
         {
             return Marshal.SizeOf<IntPtr>();
         }
 
-        [NativeImpl(typeof(long), TypeName, "objectFieldOffset", typeof(FieldInfo))]
-        [NativeImpl(typeof(long), TypeName, "staticFieldOffset", typeof(FieldInfo))]
+        [NativeImpl, NativeImpl(MethodName = "staticFieldOffset")]
         public static long objectFieldOffset(object @this, FieldInfo fi)
         {
             return Marshal.OffsetOf(fi.DeclaringType, fi.Name).ToInt64();
         }
 
-        [NativeImpl(typeof(int), TypeName, "getIntVolatile", typeof(object), typeof(long))]
-        [NativeImpl(typeof(int), TypeName, "getInt", typeof(object), typeof(long))]
+        [NativeImpl, NativeImpl(MethodName = "getIntVolatile")]
         public static int getInt(object @this, object ptr, long offset)
         {
             return Marshal.ReadInt32(ptr, (int)offset);
         }
 
-        [NativeImpl(typeof(void), TypeName, "putIntVolatile", typeof(object), typeof(long), typeof(int))]
-        [NativeImpl(typeof(void), TypeName, "putInt", typeof(object), typeof(long), typeof(int))]
+        [NativeImpl, NativeImpl(MethodName = "putIntVolatile")]
         public static void putInt(object @this, object ptr, long offset, int value)
         {
             Marshal.WriteInt32(ptr, (int) offset, value);
         }
 
-        [NativeImpl(typeof(int), TypeName, "getInt", typeof(long))]
+        [NativeImpl]
         public static int getInt(object @this, long ptr)
         {
             return Marshal.ReadInt32(new IntPtr(ptr));
         }
 
-        [NativeImpl(typeof(void), TypeName, "putInt", typeof(long), typeof(int))]
+        [NativeImpl]
         public static void putInt(object @this, long ptr, int value)
         {
             Marshal.WriteInt32(new IntPtr(ptr), value);
         }
 
-        [NativeImpl(typeof(long), TypeName, "getLongVolatile", typeof(object), typeof(long))]
-        [NativeImpl(typeof(long), TypeName, "getLong", typeof(object), typeof(long))]
+        [NativeImpl, NativeImpl(MethodName = "getObjectVolatile")]
+        public static object getObject(object @this, object ptr, long offset)
+        {
+            if (ptr is Array arr)
+            {
+                return arr.GetValue(offset / arrayIndexScale(@this, ptr.GetType()));
+            }
+            
+            return ptr.GetType().GetRuntimeFields()
+                .First(f => Marshal.OffsetOf(ptr.GetType(), f.Name).ToInt64() == offset)
+                .GetValue(ptr);
+        }
+
+        [NativeImpl, NativeImpl(MethodName = "putObjectVolatile")]
+        public static void putObject(object @this, object ptr, long offset, object value)
+        {
+            if (ptr is Array arr)
+            {
+                arr.SetValue(value, offset / arrayIndexScale(@this, ptr.GetType()));
+            }
+            else
+            {
+                ptr.GetType().GetRuntimeFields()
+                    .First(f => Marshal.OffsetOf(ptr.GetType(), f.Name).ToInt64() == offset)
+                    .SetValue(ptr, value);
+            }
+        }
+
+        [NativeImpl, NativeImpl(MethodName = "getBooleanVolatile")]
+        public static bool getBoolean(object @this, object ptr, long offset)
+        {
+            return getByte(@this, ptr, offset) != 0;
+        }
+
+        [NativeImpl, NativeImpl(MethodName = "putBooleanVolatile")]
+        public static void putBoolean(object @this, object ptr, long offset, bool value)
+        {
+            putByte(@this, ptr, offset, (sbyte) (value ? 1 : 0));
+        }
+
+        [NativeImpl, NativeImpl(MethodName = "getLongVolatile")]
         public static long getLong(object @this, object ptr, long offset)
         {
             return Marshal.ReadInt64(ptr, (int)offset);
         }
 
-        [NativeImpl(typeof(void), TypeName, "putLongVolatile", typeof(object), typeof(long), typeof(long))]
-        [NativeImpl(typeof(void), TypeName, "putLong", typeof(object), typeof(long), typeof(long))]
+        [NativeImpl, NativeImpl(MethodName = "putLongVolatile")]
         public static void putLong(object @this, object ptr, long offset, long value)
         {
             Marshal.WriteInt64(ptr, (int)offset, value);
         }
 
-        [NativeImpl(typeof(long), TypeName, "getLong", typeof(long))]
+        [NativeImpl]
         public static long getLong(object @this, long ptr)
         {
             return Marshal.ReadInt64(new IntPtr(ptr));
         }
 
-        [NativeImpl(typeof(void), TypeName, "putLong", typeof(long), typeof(long))]
+        [NativeImpl]
         public static void putLong(object @this, long ptr, long value)
         {
             Marshal.WriteInt64(new IntPtr(ptr), value);
         }
 
-        [NativeImpl(typeof(sbyte), TypeName, "getByteVolatile", typeof(object), typeof(long))]
-        [NativeImpl(typeof(sbyte), TypeName, "getByte", typeof(object), typeof(long))]
-        public static long getByte(object @this, object ptr, long offset)
+        [NativeImpl, NativeImpl(MethodName = "getByteVolatile")]
+        public static sbyte getByte(object @this, object ptr, long offset)
         {
             return (sbyte)Marshal.ReadByte(ptr, (int)offset);
         }
 
-        [NativeImpl(typeof(void), TypeName, "putByteVolatile", typeof(object), typeof(long), typeof(sbyte))]
-        [NativeImpl(typeof(void), TypeName, "putByte", typeof(object), typeof(long), typeof(sbyte))]
+        [NativeImpl, NativeImpl(MethodName = "putByteVolatile")]
         public static void putByte(object @this, object ptr, long offset, sbyte value)
         {
             Marshal.WriteByte(ptr, (int) offset, (byte) value);
         }
 
-        [NativeImpl(typeof(sbyte), TypeName, "getByte", typeof(long))]
+        [NativeImpl]
         public static sbyte getByte(object @this, long ptr)
         {
             return (sbyte)Marshal.ReadByte(new IntPtr(ptr));
         }
 
-        [NativeImpl(typeof(void), TypeName, "putByte", typeof(long), typeof(sbyte))]
+        [NativeImpl]
         public static void putByte(object @this, long ptr, sbyte value)
         {
             Marshal.WriteByte(new IntPtr(ptr), (byte) value);
         }
 
-        [NativeImpl(typeof(bool), TypeName, "compareAndSwapInt", typeof(object), typeof(long), typeof(int), typeof(int))]
+        [NativeImpl]
         public static bool compareAndSwapInt(object @this, object ptr, long offset, int value, int original)
         {
             lock (ptr)
@@ -134,19 +168,41 @@ namespace JavaNet.Runtime.Plugs.NativeImpl
             }
         }
 
-        [NativeImpl(typeof(long), TypeName, "allocateMemory", typeof(long))]
+        [NativeImpl]
+        public static bool compareAndSwapObject(object @this, object ptr, long offset, object value, object original)
+        {
+            lock (ptr)
+            {
+                var read = getObject(@this, ptr, offset);
+                putObject(@this, ptr, offset, value);
+                return read != original;
+            }
+        }
+
+        [NativeImpl]
+        public static bool compareAndSwapLong(object @this, object ptr, long offset, long value, long original)
+        {
+            lock (ptr)
+            {
+                var read = getLong(@this, ptr, offset);
+                putLong(@this, ptr, offset, value);
+                return read != original;
+            }
+        }
+
+        [NativeImpl]
         public static long allocateMemory(object @this, long size)
         {
             return Marshal.AllocHGlobal(new IntPtr(size)).ToInt64();
         }
 
-        [NativeImpl(typeof(void), TypeName, "freeMemory", typeof(long))]
+        [NativeImpl]
         public static void freeMemory(object @this, long ptr)
         {
             Marshal.FreeHGlobal(new IntPtr(ptr));
         }
 
-        [NativeImpl(typeof(long), TypeName, "reallocateMemory", typeof(long), typeof(long))]
+        [NativeImpl]
         public static long reallocateMemory(object @this, long ptr, long newSize)
         {
             return Marshal.ReAllocHGlobal(new IntPtr(ptr), new IntPtr(newSize)).ToInt64();
